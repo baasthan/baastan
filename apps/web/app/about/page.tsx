a9ad17c739d6d35d0917da2cce382409fb29c10b
Baasthan <baasthan@gmail.com>
2025-06-04 04:44:17 +0530

    fix(about-us): fix alignment of text and images

--- apps/web/app/about/page.tsx
+++ apps/web/app/about/page.tsx
@@ -12,20 +12,20 @@ function About() {
   return (
     <div className="relative flex flex-col items-center gap-y-10 p-2">
       <h1 className="text-4xl font-bold capitalize">About Us</h1>
-      <div className="md:max-w-lg flex flex-row flex-row-reverse items-center">
-        <p className="leading-7 [&:not(:first-child)]:mt-6 text-right">
+      <div className="md:max-w-lg flex flex-col-reverse  md:flex-row-reverse items-center">
+        <p className="leading-7 [&:not(:first-child)]:mt-6  text-center md:text-right">
           Baasthan is a smart digital housing platform designed to help students
           and working professionals find affordable and verified PGs and rental
           accomodations with ease.
         </p>
         <Image
           src={SmartHome}
-          className="w-1/3 mx-10"
+          className="w-1/3 md:mx-10"
           alt="smart digital housing"
         />
       </div>
-      <div className="md:max-w-lg flex flex-row flex-row items-center">
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <div className="md:max-w-lg flex flex-col-reverse md:flex-row items-center">
+        <p className="leading-7 [&:not(:first-child)]:mt-6 text-center md:text-left">
           With zero brokerage and no hidden fees, Baasthan simplifies the
           process of finding a place you can call home. Through out
           user-friendly app , users can browse, compare and book accomodations
@@ -34,17 +34,21 @@ function About() {
         <Image
           src={DigitalHome}
           alt="Digital Marketplace"
-          className="w-1/3 mx-10"
+          className="w-1/3 md:mx-10"
         />
       </div>
-      <div className="md:max-w-lg flex flex-row-reverse items-center">
-        <p className="leading-7 [&:not(:first-child)]:mt-6">
+      <div className="md:max-w-lg flex flex-col-reverse md:flex-row-reverse items-center">
+        <p className="leading-7 [&:not(:first-child)]:mt-6  text-center md:text-right">
           At the same time, Baasthan empowers property owners by helping them
           efficiently manage, list and operate their properties through a
           dedicated dashboard - ensuring better occupancy, visibility and
           operational control
         </p>
-        <Image src={ListHome} alt="list properties" className="w-1/3 mx-10" />
+        <Image
+          src={ListHome}
+          alt="list properties"
+          className="w-1/3 md:mx-10"
+        />
       </div>
     </div>
   );
