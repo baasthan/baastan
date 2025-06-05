9b82b79c29f7fd0e53e878bb05f082347fd8e8a9
Rishi Mukherjee <rishi.mukherjee.baasthan@gmail.com>
2025-06-05 22:28:43 +0530

    feat(sitemap): added sitemap

---
+++ apps/web/app/sitemap.ts
@@ -0,0 +1,25 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  /**
+   * Note: This needs to be changed while moving to any other hosting provider. This value should be available during build time
+   */
+  const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/`
+    : "http://localhost:3000/";
+
+  return [
+    {
+      url: baseUrl,
+      changeFrequency: "hourly",
+      lastModified: new Date().toISOString(),
+      priority: 1,
+    },
+    {
+      url: `${baseUrl}/about`,
+      changeFrequency: "hourly",
+      lastModified: new Date().toISOString(),
+      priority: 1,
+    },
+  ];
+}
