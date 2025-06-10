ff49a7303b5a59008ede3166916be5e28a76b3fd
Rishi Mukherjee <rishi.mukherjee.baasthan@gmail.com>
2025-06-10 23:09:02 +0530

    Feat/footer (#26)
    * fix(favicon): update favicon (#14)
    
    Co-authored-by: Rishi Mukherjee <rishi.mukherjee.baasthan@gmail.com>
    
    * added footer
    
    * updated footer
    
    * fix(footer): minor changes in footer
    
    ---------
    
    Co-authored-by: baasthan <baasthan@gmail.com>
    Co-authored-by: abhigyan2003 <abhigyanborah3@gmail.com>

--- apps/web/components/app-shell.tsx
+++ apps/web/components/app-shell.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { Toaster } from "@workspace/ui/components/sonner";
-import Footer from "./footer";
+import React from "react";
 import TopNav from "./top-nav";
 
 interface AppShellProps {
@@ -14,9 +13,11 @@ const AppShell = ({ children }: AppShellProps) => {
       <header className="sticky top-0 bg-white z-10">
         <TopNav />
       </header>
-      <Toaster position="top-right" />
-      <main className="max-w-7xl mx-auto w-full px-4 flex-1">{children}</main>
-      <Footer />
+
+      <main className="max-w-6xl mx-auto max-h-[calc(100svh-3.5rem)] p-2 lg:p-0">
+        {children}
+      </main>
+      <footer></footer>
     </>
   );
 };
