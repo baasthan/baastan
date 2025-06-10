de25e73baf6b1e4a2561af2aa147841c4baea33a
Rishi Mukherjee <rishi.mukherjee.baasthan@gmail.com>
2025-06-10 21:56:24 +0530

    feat(home-animation): implement basic animations on homescreen (#19)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,11 @@
+import HomeHero1 from "@/components/home-hero-1";
+import HomeHero2 from "@/components/home-hero-2";
+
 export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Under Construction 🚧</h1>
-      </div>
-    </div>
+    <>
+      <HomeHero1 />
+      <HomeHero2 />
+    </>
   );
 }
