273ec0fdcb94737bf62f38204f20d6bfd011229b
anushree-baasthan <anushree.tyagi.baasthan@gmail.com>
2025-06-12 02:15:21 +0530

    feat(transitions): add animations (#33)
    Co-authored-by: akagi <shigeruakagi07@gmail.com>

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -15,6 +15,7 @@ export default function Page() {
         <HomeHero2 />
         <Amenities />
       </div>
+
       {/* Amenities Card - Bottom Right */}
 
       {/* Why Baasthan - Bottom Left */}
