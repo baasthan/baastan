a1f02b5352f2a19efa7001729087351f2bfbd2bf
rishi-baasthan <rishi.mukherjee.baasthan@gmail.com>
2025-07-12 17:34:59 +0530

    fix(sitemap): fix sitemap urls

--- apps/web/app/sitemap.ts
+++ apps/web/app/sitemap.ts
@@ -5,19 +5,19 @@ export default function sitemap(): MetadataRoute.Sitemap {
    * Note: This needs to be changed while moving to any other hosting provider. This value should be available during build time
    */
   const baseUrl = process.env.VERCEL_PROJECT_PRODUCTION_URL
-    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}/`
-    : "http://localhost:3000/";
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : "http://localhost:3000";
 
   return [
     {
       url: baseUrl,
-      changeFrequency: "hourly",
+      changeFrequency: "daily",
       lastModified: new Date().toISOString(),
       priority: 1,
     },
     {
       url: `${baseUrl}/about`,
-      changeFrequency: "hourly",
+      changeFrequency: "daily",
       lastModified: new Date().toISOString(),
       priority: 1,
     },
