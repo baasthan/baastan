07d7e62ac83b77deb76efa041376de20e8c75c91
rishi-baasthan <rishi.mukherjee.baasthan@gmail.com>
2025-07-17 04:00:20 +0530

    feat(properties): work in progress

--- apps/backend/lib/prismaClient.ts
+++ apps/backend/lib/prismaClient.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@/generated/prisma";
 import { withOptimize } from "@prisma/extension-optimize";
 
 const getPrismaClient = () => {
+  console.log("process.env.DATABASE_URL===>", process.env.DATABASE_URL);
   const prismaClient = new PrismaClient().$extends(
     withOptimize({ apiKey: process.env.OPTIMIZE_API_KEY! })
   );
